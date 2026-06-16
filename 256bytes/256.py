376969dabcd89b44da937255d5627a858cb60fe1
Carlo Minucci <gecco@napodano.com>
2026-06-16 09:41:45 +0200

    Butto su la entry per la 256 byte

---
+++ 256bytes/256.py
@@ -0,0 +1,6 @@
+import pygame as p
+p.init()
+s=p.display.set_mode((2,2),p.FULLSCREEN)
+c=99
+f=p.font.SysFont(0,c)
+for x in range(700):t=f.render('adsux',0,(c,x/3,x/c),1);p.draw.line(s,(x/3,0,c),(c,x*2),(x,x/7));s.blit(t,(x,c));p.display.flip();p.event.wait(40)
